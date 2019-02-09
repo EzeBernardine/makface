84825584f8a01c932e15a02daa41954ec3bcd817
EzeBernardine <demayous1@gmail.com>
2019-02-09 08:56:31 +0100

    skeletal UI

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './Landing.css';
-// import FaAlignRight from 'react-icons';
-// import FaIconPack from 'react-icons'
 
 class Landing extends Component {
   render() {
     return (
-      <div className='container'>
-        {/* <FaIconPack /> */}
+      <div className='container middle'>
+        <div className='logo middle '>
+          <h1 className='logoIcon'>p</h1>
+        </div>
+
+        <div className='header'>
+          <p className='description'>Show the world what you have , an they will tell you what they want</p>
+          <h1 className='name'>pACT</h1>
+          <p className='theme'>Enhance your buiness</p>
+          <button  className='login bButton'>Login</button>
+        </div>
+
+        <div className='footer'> footer </div>
       </div>
     );
   }
