b06ad8a7ea8bcf263f974afcf2362d5c930fb7f4
EzeBernardine <demayous1@gmail.com>
2019-02-09 07:43:13 +0100

    creating the first page

---
+++ src/component/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Landing.css';
+// import FaAlignRight from 'react-icons';
+// import FaIconPack from 'react-icons'
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className='container'>
+        {/* <FaIconPack /> */}
+      </div>
+    );
+  }
+}
+
+
+export default Landing;
