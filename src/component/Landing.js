b468d2a6de18ad246372120f8c4ca411e37b983a
EzeBernardine <demayous1@gmail.com>
2019-02-10 04:33:34 +0100

    Complete landing page

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import './Landing.css';
+import { IoLogoWhatsapp , IoLogoFacebook} from "react-icons/io";
+import { FaTwitterSquare } from "react-icons/fa";
 
 class Landing extends Component {
   render() {
     return (
       <div className='container middle'>
-        <div className='logo middle '>
-          <h1 className='logoIcon'>p</h1>
+        <div className='logoDiv'>
+          <div className='logo middle '>
+            <h1 className='logoIcon font-effect-3d-float'>p</h1>
+          </div>
         </div>
 
-        <div className='header'>
-          <p className='description'>Show the world what you have , an they will tell you what they want</p>
-          <h1 className='name'>pACT</h1>
-          <p className='theme'>Enhance your buiness</p>
+        <div className='header '>
+          <p className='description '>Show the world what you have, and learn what they want</p>
+          <h1 className='name font-effect-shadow-multiple'>pACT</h1>
           <button  className='login bButton'>Login</button>
         </div>
 
-        <div className='footer'> footer </div>
+        <div className='footer'> 
+          <p className='theme '>Enhance your buiness</p>
+          <div className='icons'> 
+            <span><IoLogoWhatsapp /></span> <span><IoLogoFacebook/></span> 
+            <span><FaTwitterSquare /></span> 
+          </div>
+        </div>
       </div>
     );
   }
